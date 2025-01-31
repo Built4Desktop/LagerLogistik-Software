eea4ac5065b9579e7f2904537f53ba34fc7d8431
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-01-31 23:30:32 +0100

    Icon + logo added
    Not ideal implementation but had some trouble with the java "searching"

--- Oranje-Logistik-Software/src/main/java/org/example/oranjelogistiksoftware/OranjeLogistik.java
+++ Oranje-Logistik-Software/src/main/java/org/example/oranjelogistiksoftware/OranjeLogistik.java
@@ -6,13 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Objects;
+import java.io.InputStream;
+
 
 public class OranjeLogistik extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(OranjeLogistik.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        Image image = new Image(getClass().getResource("/org/example/oranjelogistiksoftware/images/logo.png").toExternalForm());
+        stage.getIcons().add(image);
         stage.setTitle("Oranje Logistik");
         stage.setScene(scene);
         stage.show();
